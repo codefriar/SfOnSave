0df425daa0887e03ae000a6bcaa9c2742a2ce104
Kevin Poorman <kjp@codefriar.com>
2018-11-01 12:04:36 -0400

    incremental save.

--- src/extension.ts
+++ src/extension.ts
@@ -7,28 +7,57 @@ let appName       = "SfOnSave";
 let outputChannel = vscode.window.createOutputChannel(appName);
 
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  let config = new Config();
-
-  if (!config.sfdxCommand) {
-    config.sfdxCommand = "sfdx force:source:deploy --sourcepath ";
-  }
+  var sfdxOnSave = new SfOnSaveExtension(context);
 
   vscode.workspace.onDidSaveTextDocument(textDocument => {
-    ChildProcess.exec(
-      `cd "${vscode.workspace.rootPath}" && ${config.sfdxCommand} "${
-        textDocument.fileName
-      }"`,
-      (err, stdout, stderr) => {
-        if (err) {
-          outputChannel.append(stderr);
-        } else {
-          outputChannel.append(stdout);
-        }
-        outputChannel.show(true);
-      }
-    );
+    sfdxOnSave.run(textDocument);
   });
+}
+
+class SfOnSaveExtension {
+  private _config = this.checkConfig();
+
+  constructor(context: vscode.ExtensionContext) {
+    this.checkConfig();
+  }
+
+  public checkConfig(): Config {
+    let config = new Config();
+
+    if (!config.sfdxCommand) {
+      config.sfdxCommand = "sfdx force:source:deploy --sourcepath ";
+    }
+
+    if (!config.sfdxProjectRoot) {
+      config.sfdxProjectRoot = "/force-app";
+    }
+    return config;
+  }
+
+  public run(textDocument: vscode.TextDocument): void {
+    console.log(textDocument.fileName);
+    if (textDocument.fileName.startsWith(this._config.sfdxProjectRoot)) {
+      ChildProcess.exec(
+        `cd "${vscode.workspace.rootPath}" && ${this._config.sfdxCommand} "${
+          textDocument.fileName
+        }"`,
+        (err, stdout, stderr) => {
+          if (err) {
+            outputChannel.append(stderr);
+          } else {
+            outputChannel.append(stdout);
+          }
+        }
+      );
+    } else {
+      outputChannel.append(
+        `Changed file is outside of SFDX Project Root (${
+          this._config.sfdxProjectRoot
+        }), and was not deployed`
+      );
+    }
+    outputChannel.show(true);
+  }
 }
 
 // this method is called when your extension is deactivated
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,10 +2,12 @@
 import * as vscode from "vscode";
 
 export class Config {
-  public sfdxCommand: string;
+  public sfdxCommand    : string;
+  public sfdxProjectRoot: string;
 
   public constructor() {
-    let config           = vscode.workspace.getConfiguration("SfOnSave");
-        this.sfdxCommand = config.get("sfdxCommand") as string;
+    let config               = vscode.workspace.getConfiguration("SfOnSave");
+        this.sfdxCommand     = config.get("sfdxCommand") as string;
+        this.sfdxProjectRoot = config.get("sfdxProjectRoot") as string;
   }
 }
